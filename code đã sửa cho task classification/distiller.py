8b434c959d433128cd0966baf463cb9d3469fe9b
haian1910 <145516487+haian1910@users.noreply.github.com>
2025-03-31 11:52:25 +0700

    Update distiller.py

--- code đã sửa cho task classification/distiller.py
+++ code đã sửa cho task classification/distiller.py
@@ -61,6 +61,66 @@ class Distiller(nn.Module):
         tokenizer.pad_token_id = tokenizer.eos_token_id
         
         return tokenizer
+    def set_and_load_existing_projectors(self):
+        self.projectors = nn.ModuleDict()
+        projector_config = json.load(open(self.args.projector_config_path))
+        name_dict = {
+            "s": self.student_hidden_size, 
+            "t": self.teacher_hidden_size,
+            "relu": nn.ReLU()
+        }
+        # auto-parse projector config strings to construct nn.Module
+        for projector_name in projector_config:
+            # for d in projector_config[loc]:
+            if projector_config[projector_name]["enabled"]:
+                self.projectors[projector_name] = nn.Sequential()
+
+                structure = projector_config[projector_name]["structure"].split("-")
+                for i in range(len(structure)):
+                    if structure[i] not in ["relu"]:
+                        coef = 1 if not len(structure[i][:-1]) else int(structure[i][:-1])
+                        base_size = name_dict[structure[i][-1]]
+                        structure[i] = coef * base_size
+
+                for i in range(len(structure) - 1):
+                    if isinstance(structure[i], int) and isinstance(structure[i+1], int):
+                        self.projectors[projector_name].append(
+                            nn.Linear(structure[i], structure[i+1])
+                        )
+                    elif isinstance(structure[i], int) and isinstance(structure[i+1], str):
+                        self.projectors[projector_name].append(
+                            name_dict[structure[i+1]]
+                        )
+                        last_size = structure[i]
+                    elif isinstance(structure[i], str) and isinstance(structure[i+1], int):
+                        self.projectors[projector_name].append(
+                            nn.Linear(last_size, structure[i+1])
+                        )
+                    else:
+                        raise NotImplementedError(f"Invalid structure for '{structure}'")
+                        
+        # load existing projectors if already have
+        self.load_existing_projectors()
+
+    def load_existing_projectors(self):
+        if self.args.projector_path is not None:
+            projector_path = os.path.join(self.args.projector_path, "projector.pt")
+        else:
+            projector_path = os.path.join(self.args.model_path, "projector.pt")
+
+        if os.path.exists(projector_path):
+            projector_params = torch.load(projector_path, map_location=f"cuda:{self.device}")
+            log_rank("Existing projector params: {}".format(list(projector_params.keys())))
+            for key in self.projectors:
+                try:
+                    state_dict = {
+                        n.split('.', 1)[1]: projector_params[n] for n in projector_params if n.startswith(key)
+                    }
+                    self.projectors[key].load_state_dict(state_dict)
+                    log_rank("Load projector '{}' from current path.".format(key))
+                except:
+                    log_rank("Not compatible for projector '{}'".format(key))
+                    continue
     
     def load_student_model(self):
         log_rank("Loading student model...")
